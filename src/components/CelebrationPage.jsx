18e6c41be1e914860a51f5d3f6f9bdd4f7c0978a
sociolearning08-oss <sociolearning08@gmail.com>
2025-12-18 14:53:18 +0530

    Update CelebrationPage.jsx

--- src/components/CelebrationPage.jsx
+++ src/components/CelebrationPage.jsx
@@ -44,7 +44,7 @@ function CelebrationPage({ onComplete, musicPlayerRef }) {
     },
     {
       icon: "✨",
-      text: "Have a look at it, Madam Jiii",
+      text: "Have a look at it, My Golgappa! ",
       type: "announcement",
     },
   ];
